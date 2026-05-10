c78a2daa7c7faaed2439bfc59677bdded5f1678e
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-05-11 00:17:42 +0400

    fix: convert nbsp to regular space in rich text display so Georgian text wraps at word boundaries

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v11";
+const CACHE_NAME = "spiningebi-v12";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
