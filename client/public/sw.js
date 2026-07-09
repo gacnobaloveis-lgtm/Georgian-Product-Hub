ec61d0b2f78079dce22830579e5dd597aa5ca94c
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-07-09 20:08:32 +0400

    Add per-product purchase limit: client/public/sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v16";
+const CACHE_NAME = "spiningebi-v17";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
