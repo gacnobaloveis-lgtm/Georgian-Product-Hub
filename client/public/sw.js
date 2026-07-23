3bd58817f2ad7f7d88634852a524215cfedda974
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-07-23 20:52:43 +0400

    deploy: chest promo feature (client/public/sw.js)

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v23";
+const CACHE_NAME = "spiningebi-v24";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
