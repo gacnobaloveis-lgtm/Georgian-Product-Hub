af7186c5e368cf4d50b10fe8b0e70abe3e7fbace
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-07-23 22:56:28 +0400

    deploy: chest one-time-per-product (client/public/sw.js)

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v28";
+const CACHE_NAME = "spiningebi-v29";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
