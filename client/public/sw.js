ab354a4fb3f996edba6a5851c8757cd0214f0672
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-07-23 22:36:56 +0400

    deploy: chest popup carousel + timer fix (client/public/sw.js)

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v26";
+const CACHE_NAME = "spiningebi-v27";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
