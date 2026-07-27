263fbc0982a59390f9efca2f2b9ecd7a32f00477
gacnobaloveis-lgtm <gacnobaloveis@gmail.com>
2026-07-28 02:52:37 +0400

    deploy: client/public/sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spiningebi-v37";
+const CACHE_NAME = "spiningebi-v38";
 const STATIC_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".ico", ".woff", ".woff2", ".ttf"];
 
 self.addEventListener("install", () => {
